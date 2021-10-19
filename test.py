73ad1c6ef44382cc2632b1e50175054cd35f061c
afra amini <afra.amini@gmail.com>
2021-10-19 13:44:17 +0200

    Reverse some changes in leftcorner transform code

--- test.py
+++ test.py
@@ -7,18 +7,18 @@ from leftcorner_rev_transformer import rev_transform
 root = Node(NodeInfo(NodeType.NT, "S"), None)
 np = Node(NodeInfo(NodeType.NT, "NP"), root)
 vp = Node(NodeInfo(NodeType.NT, "VP"), root)
-root.set_left(vp)
-root.set_right(np)
+root.set_left(np)
+root.set_right(vp)
 
 det = Node(NodeInfo(NodeType.PT, "Det(the)"), np)
 n = Node(NodeInfo(NodeType.PT, "N(dog)"), np)
-np.set_left(n)
-np.set_right(det)
+np.set_left(det)
+np.set_right(n)
 
 v = Node(NodeInfo(NodeType.PT, "V(ran)"), vp)
 adv = Node(NodeInfo(NodeType.PT, "Adv(fast)"), vp)
-vp.set_left(adv)
-vp.set_right(v)
+vp.set_left(v)
+vp.set_right(adv)
 
 new_root = Node(NodeInfo(NodeType.NT, "S", ref=root), None)
 
@@ -27,8 +27,6 @@ print_tree(root, nameattr='label', left_child='left', right_child='right')
 left_corner_transform(new_root)
 
 print_tree(new_root, nameattr='label', left_child='left', right_child='right')
-
-exit(0)
 
 rev_new_root = rev_transform(new_root)
 while rev_new_root.parent is not None:
