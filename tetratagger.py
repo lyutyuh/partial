16f0c85b8cb9220955c9ad3ac53fd6b1325a7baf
Ryan Cotterell <rdc42@cst.cam.ac.uk>
2021-10-19 14:17:24 +0200

    minor

--- tetratagger.py
+++ tetratagger.py
@@ -16,14 +16,17 @@ class TopDownTetratagger(object):
 			node = stack.pop()
 
 			if node.node_info.type == NodeType.NT or node.node_info.type == NodeType.NT_NT:				
-				print("REDUCE[ {0} --> {1} {2}]".format(*(node.label, node.left.label, node.right.label)))
 				if not eps(node.right):
 					stack.append(node.right)
 				if not eps(node.left):
 					stack.append(node.left)
+
+				print("<==\tREDUCE[ {0} --> {1} {2}]".format(*(node.label, node.left.label, node.right.label)))
+
+
 				print(stack); print()
 			elif node.node_info.type == NodeType.PT:
-				print("SHIFT[ {0} ]".format(node.label))
+				print("<--\tSHIFT[ {0} ]".format(node.label))
 				print(stack); print()
 
 			#print(node.label)
