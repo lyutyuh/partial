4b45e10b42b843f27316f00998cbbed1b8aa0d3f
Ryan Cotterell <rdc42@cst.cam.ac.uk>
2021-10-19 10:46:35 +0200

    started arc eager; noticed bug with unary rules

--- arc_eager.py
+++ arc_eager.py
@@ -1,3 +1,7 @@
+from ppbtree import print_tree
+
+from node import Node, NodeType, NodeInfo
+from leftcorner_transformer import left_corner_transform, right_corner_transform
 
 
 class ArcEager(object):
@@ -10,4 +14,42 @@ class ArcEager(object):
 	def arc_eager(self, tree):
 		pass
 
-	
+	def shift_reduce(self,tree):
+		stack = []
+
+		pass
+
+
+root = Node(NodeInfo(NodeType.NT, "Z_c"), None)
+Y_b = Node(NodeInfo(NodeType.NT, "Y_b"), root)
+Y_c = Node(NodeInfo(NodeType.NT, "Y_c"), root)
+root.set_left(Y_b)
+root.set_right(Y_c)
+
+X_a = Node(NodeInfo(NodeType.PT, "X_a"), Y_b)
+X_b = Node(NodeInfo(NodeType.PT, "X_b"), Y_b)
+Y_b.set_left(X_a)
+Y_b.set_right(X_b)
+
+a = Node(NodeInfo(NodeType.PT, "a"), X_a)
+X_a.set_right(a)
+b = Node(NodeInfo(NodeType.PT, "b"), X_b)
+X_b.set_right(b)
+
+X_c = Node(NodeInfo(NodeType.PT, "X_c"), Y_c)
+X_d = Node(NodeInfo(NodeType.PT, "X_d"), Y_c)
+Y_c.set_left(X_c)
+Y_c.set_right(X_d)
+
+c = Node(NodeInfo(NodeType.PT, "c"), X_c)
+X_c.set_right(c)
+d = Node(NodeInfo(NodeType.PT, "d"), X_d)
+X_d.set_right(d)
+
+print_tree(root, nameattr='label', left_child='left', right_child='right')
+
+new_root = Node(NodeInfo(NodeType.NT, "Z_c", ref=root), None)
+
+left_corner_transform(new_root)
+
+print_tree(new_root, nameattr='label', left_child='left', right_child='right')
