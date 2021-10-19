226358920b1d79b2bb59f3f93a0f40cd2e63acc3
Ryan Cotterell <rdc42@cst.cam.ac.uk>
2021-10-19 13:37:11 +0200

    updated arc eager

--- arc_eager.py
+++ arc_eager.py
@@ -23,13 +23,13 @@ class ArcEager(object):
 root = Node(NodeInfo(NodeType.NT, "Z_c"), None)
 Y_b = Node(NodeInfo(NodeType.NT, "Y_b"), root)
 Y_c = Node(NodeInfo(NodeType.NT, "Y_c"), root)
-root.set_left(Y_b)
-root.set_right(Y_c)
+root.set_left(Y_c)
+root.set_right(Y_b)
 
 X_a = Node(NodeInfo(NodeType.PT, "X_a"), Y_b)
 X_b = Node(NodeInfo(NodeType.PT, "X_b"), Y_b)
-Y_b.set_left(X_a)
-Y_b.set_right(X_b)
+Y_b.set_left(X_b)
+Y_b.set_right(X_a)
 
 a = Node(NodeInfo(NodeType.PT, "a"), X_a)
 X_a.set_right(a)
@@ -38,8 +38,8 @@ X_b.set_right(b)
 
 X_c = Node(NodeInfo(NodeType.PT, "X_c"), Y_c)
 X_d = Node(NodeInfo(NodeType.PT, "X_d"), Y_c)
-Y_c.set_left(X_c)
-Y_c.set_right(X_d)
+Y_c.set_left(X_d)
+Y_c.set_right(X_c)
 
 c = Node(NodeInfo(NodeType.PT, "c"), X_c)
 X_c.set_right(c)
