21aa9ddb5efa37913e8996f5a2e9b525fb00b570
afra amini <afra.amini@gmail.com>
2022-01-12 14:49:47 +0100

    Add evaluation pipeline

--- run.py
+++ run.py
@@ -13,6 +13,7 @@ from learning.dataset import TaggingDataset
 from learning.model import ModelForTetratagging, BertCRFModel, BertLSTMModel
 from tagging.srtagger import SRTaggerBottomUp, SRTaggerTopDown
 from tagging.tetratagger import BottomUpTetratagger
+from learning.evaluate import predict, calc_parse_eval, calc_tag_accuracy
 
 logging.getLogger().setLevel(logging.INFO)
 
@@ -31,6 +32,7 @@ MODEL_NAME = 'distilbert'
 parser = argparse.ArgumentParser()
 subparser = parser.add_subparsers(dest='command')
 train = subparser.add_parser('train')
+evaluate = subparser.add_parser('evaluate')
 
 train.add_argument('--tagger', choices=[TETRATAGGER, TD_SR, BU_SR], required=True,
                    help="Tagging schema")
@@ -46,6 +48,11 @@ train.add_argument('--lr', type=float, default=5e-5)
 train.add_argument('--epochs', type=int, default=4)
 train.add_argument('--batch-size', type=int, default=16)
 train.add_argument('--num-warmup-steps', type=int, default=160)
+
+evaluate.add_argument('--model-name', type=str, required=True)
+evaluate.add_argument('--model-path', type=str, default='pat-models/')
+evaluate.add_argument('--output-path', type=str, default='results/')
+evaluate.add_argument('--batch-size', type=int, default=16)
 
 
 def initialize_tag_system(tagging_schema):
@@ -78,7 +85,7 @@ def prepare_training_data(tag_system, tagging_schema, batch_size):
     return train_dataset, eval_dataset, train_dataloader, eval_dataloader
 
 
-def generate_config(model_type, tagging_shcema, tag_system, model_path):
+def generate_config(model_type, tagging_schema, tag_system, model_path):
     if model_type == BERTCRF or model_type == BERTLSTM:
         config = transformers.AutoConfig.from_pretrained(
             MODEL_NAME,
@@ -88,7 +95,7 @@ def generate_config(model_type, tagging_shcema, tag_system, model_path):
                 'num_tags': len(tag_system.tag_vocab),
             }
         )
-    elif model_type == BERT and tagging_shcema == TETRATAGGER:
+    elif model_type == BERT and tagging_schema == TETRATAGGER:
         config = transformers.AutoConfig.from_pretrained(
             MODEL_NAME,
             num_labels=len(tag_system.tag_vocab),
@@ -100,7 +107,7 @@ def generate_config(model_type, tagging_shcema, tag_system, model_path):
                 'num_odd_tags': tag_system.internal_tag_vocab_size,
             }
         )
-    elif model_type == BERT and tagging_shcema != TETRATAGGER:
+    elif model_type == BERT and tagging_schema != TETRATAGGER:
         config = transformers.AutoConfig.from_pretrained(
             MODEL_NAME,
             num_labels=2 * len(tag_system.tag_vocab),
@@ -189,9 +196,23 @@ def train(args):
     return model, eval_dataset, eval_dataloader
 
 
-def evaluate(eval_dataset, eval_dataloader, model):
-    pass
-
+def evaluate(args):
+    tagging_schema = args.model_name.split("-")[0]
+    logging.info("Initializing Tag System")
+    tag_system = initialize_tag_system(tagging_schema)
+    logging.info("Preparing Data")
+    _, eval_dataset, _, eval_dataloader = prepare_training_data(
+        tag_system, tagging_schema, args.batch_size)
+    model = torch.load(args.model_path + args.model_name)
+    if tagging_schema == TETRATAGGER:
+        num_leaf_labels = tag_system.leaf_tag_vocab_size
+    else:
+        num_leaf_labels = len(tag_system.tag_vocab)
+    predictions, eval_labels = predict(model, eval_dataloader, len(eval_dataset),
+                                       len(tag_system.tag_vocab), device)
+    calc_tag_accuracy(predictions, eval_labels, num_leaf_labels)
+    calc_parse_eval(predictions, eval_labels, eval_dataset, tag_system, args.output_path,
+                    args.model_name)
 
 def main():
     args = parser.parse_args()
