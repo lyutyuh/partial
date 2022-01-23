141e671903814155168b1ea47015a385f3c80316
afra amini <afra.amini@gmail.com>
2022-01-23 17:11:28 +0100

    Do parsing evaluation on the fly

--- learning/evaluate.py
+++ learning/evaluate.py
@@ -1,8 +1,13 @@
-import torch
-import numpy as np
-from tqdm import tqdm as tq
-
 import logging
+import math
+import os.path
+import re
+import subprocess
+import tempfile
+
+import numpy as np
+import torch
+from tqdm import tqdm as tq
 
 
 def report_eval_loss(model, eval_dataloader, device, n_iter, writer):
@@ -75,7 +80,8 @@ def calc_parse_eval(predictions, eval_labels, eval_dataset, tag_system, output_p
         is_word = eval_labels[i] != 0
         original_tree = eval_dataset.trees[i]
         try:  # ignore the ones that failed in unchomsky_normal_form
-            tree = tag_system.logits_to_tree(logits, original_tree.pos(), mask=is_word, max_depth=max_depth)
+            tree = tag_system.logits_to_tree(logits, original_tree.pos(), mask=is_word,
+                                             max_depth=max_depth)
         except Exception as ex:
             template = "An exception of type {0} occurred. Arguments:\n{1!r}"
             message = template.format(type(ex).__name__, ex.args)
@@ -88,11 +94,122 @@ def calc_parse_eval(predictions, eval_labels, eval_dataset, tag_system, output_p
         predicted_dev_trees.append(tree)
         gold_dev_trees.append(original_tree)
     logging.warning("Number of binarization error: {}".format(c_err))
-    save_predictions(predicted_dev_trees, output_path + model_name + "_predictions.txt")
-    save_predictions(gold_dev_trees, output_path + model_name + "_gold.txt")
+    # save_predictions(predicted_dev_trees, output_path + model_name + "_predictions.txt")
+    # save_predictions(gold_dev_trees, output_path + model_name + "_gold.txt")
+    print(evalb("EVALB", gold_dev_trees, predicted_dev_trees))
 
 
 def save_predictions(predicted_trees, file_path):
     with open(file_path, 'w') as f:
         for tree in predicted_trees:
             f.write(' '.join(str(tree).split()) + '\n')
+
+
+class FScore(object):
+    # Code from: https://github.com/mrdrozdov/self-attentive-parser-with-extra-features/blob/master/src/evaluate.py
+    def __init__(self, recall, precision, fscore, complete_match, tagging_accuracy=100):
+        self.recall = recall
+        self.precision = precision
+        self.fscore = fscore
+        self.complete_match = complete_match
+        self.tagging_accuracy = tagging_accuracy
+
+    def __str__(self):
+        if self.tagging_accuracy < 100:
+            return "(Recall={:.2f}, Precision={:.2f}, FScore={:.2f}, CompleteMatch={:.2f}, TaggingAccuracy={:.2f})".format(
+                self.recall, self.precision, self.fscore, self.complete_match,
+                self.tagging_accuracy)
+        else:
+            return "(Recall={:.2f}, Precision={:.2f}, FScore={:.2f}, CompleteMatch={:.2f})".format(
+                self.recall, self.precision, self.fscore, self.complete_match)
+
+
+def evalb(evalb_dir, gold_trees, predicted_trees, ref_gold_path=None):
+    # Code from: https://github.com/mrdrozdov/self-attentive-parser-with-extra-features/blob/master/src/evaluate.py
+    assert os.path.exists(evalb_dir)
+    evalb_program_path = os.path.join(evalb_dir, "evalb")
+    evalb_spmrl_program_path = os.path.join(evalb_dir, "evalb_spmrl")
+    assert os.path.exists(evalb_program_path) or os.path.exists(evalb_spmrl_program_path)
+
+    if os.path.exists(evalb_program_path):
+        evalb_param_path = os.path.join(evalb_dir, "nk.prm")
+    else:
+        evalb_program_path = evalb_spmrl_program_path
+        evalb_param_path = os.path.join(evalb_dir, "spmrl.prm")
+
+    assert os.path.exists(evalb_program_path)
+    assert os.path.exists(evalb_param_path)
+
+    assert len(gold_trees) == len(predicted_trees)
+    for gold_tree, predicted_tree in zip(gold_trees, predicted_trees):
+        gold_leaves = list(gold_tree.leaves())
+        predicted_leaves = list(predicted_tree.leaves())
+        assert len(gold_leaves) == len(predicted_leaves)
+        assert all(
+            gold_leaf.word == predicted_leaf.word
+            for gold_leaf, predicted_leaf in zip(gold_leaves, predicted_leaves))
+
+    temp_dir = tempfile.TemporaryDirectory(prefix="evalb-")
+    gold_path = os.path.join(temp_dir.name, "gold.txt")
+    predicted_path = os.path.join(temp_dir.name, "predicted.txt")
+    output_path = os.path.join(temp_dir.name, "output.txt")
+
+    with open(gold_path, "w") as outfile:
+        if ref_gold_path is None:
+            for tree in gold_trees:
+                outfile.write("{}\n".format(tree.linearize()))
+        else:
+            # For the SPMRL dataset our data loader performs some modifications
+            # (like stripping morphological features), so we compare to the
+            # raw gold file to be certain that we haven't spoiled the evaluation
+            # in some way.
+            with open(ref_gold_path) as goldfile:
+                outfile.write(goldfile.read())
+
+    with open(predicted_path, "w") as outfile:
+        for tree in predicted_trees:
+            outfile.write("{}\n".format(tree.linearize()))
+
+    command = "{} -p {} {} {} > {}".format(
+        evalb_program_path,
+        evalb_param_path,
+        gold_path,
+        predicted_path,
+        output_path,
+    )
+    subprocess.run(command, shell=True)
+
+    fscore = FScore(math.nan, math.nan, math.nan, math.nan)
+    with open(output_path) as infile:
+        for line in infile:
+            match = re.match(r"Bracketing Recall\s+=\s+(\d+\.\d+)", line)
+            if match:
+                fscore.recall = float(match.group(1))
+            match = re.match(r"Bracketing Precision\s+=\s+(\d+\.\d+)", line)
+            if match:
+                fscore.precision = float(match.group(1))
+            match = re.match(r"Bracketing FMeasure\s+=\s+(\d+\.\d+)", line)
+            if match:
+                fscore.fscore = float(match.group(1))
+            match = re.match(r"Complete match\s+=\s+(\d+\.\d+)", line)
+            if match:
+                fscore.complete_match = float(match.group(1))
+            match = re.match(r"Tagging accuracy\s+=\s+(\d+\.\d+)", line)
+            if match:
+                fscore.tagging_accuracy = float(match.group(1))
+                break
+
+    success = (
+            not math.isnan(fscore.fscore) or
+            fscore.recall == 0.0 or
+            fscore.precision == 0.0)
+
+    if success:
+        temp_dir.cleanup()
+    else:
+        print("Error reading EVALB results.")
+        print("Gold path: {}".format(gold_path))
+        print("Predicted path: {}".format(predicted_path))
+        print("Output path: {}".format(output_path))
+
+    return fscore
